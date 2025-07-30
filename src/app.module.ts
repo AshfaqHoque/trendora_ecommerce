e9bb413e01446cd44578f5087d14fcbc35f6099c
Lenovo <Lenovo@LAPTOP-I88LKROM>
2025-07-30 11:21:30 +0600

    Add validation for customer DTO and fix controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/user.module';
+
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [AdminModule, UserModule],
+  imports: [AdminModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/customer/customer.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Put, Param, Delete, ValidationPipe, UsePipes } from '@nestjs/common';
+// import { CreateCustomerDto } from './dto/createCustomer.dto';
+import { CreateCustomerDto } from './create-customer.dto';
+import { CustomerService } from './customer.service';
+
+@Controller('customer')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  // @Post()
+  // create(@Body() body: any) {
+  //   return this.customerService.create(body);
+  // }
+
+  @Get()
+  findAll() {
+    return this.customerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.customerService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.customerService.update(+id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.customerService.remove(+id);
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+    console.log('DTO body received:', createCustomerDto);
+    return {
+      message: 'User created',
+      data: createCustomerDto,
+    };
+  }
+}
